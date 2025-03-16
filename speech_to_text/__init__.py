82fd3009edea4f13ac0cbc825b406fd460cecdb6
Oluwasegun Adewola <51415200+SegunAdewola@users.noreply.github.com>
2025-03-15 22:27:56 -0700

    fix- corrcted import errors
    Corrected impoorting from the `speech_to_text` package

--- speech_to_text/__init__.py
+++ speech_to_text/__init__.py
@@ -1,3 +1,8 @@
 """
-Speech To Text package initialization
+Speech to text package for audio transcription
 """
+
+from .speech_to_text import SpeechToText
+from .audio_recorder import AudioRecorder
+
+__all__ = ['SpeechToText', 'AudioRecorder']
